71e9dac6ea9d3eb35eb15378937d87d740216dda
Rafa Areses Brackenbury <rafaelareses@gmail.com>
2026-06-09 23:50:53 +0200

    Update theme from Figma - 6/9/2026

---
+++ theme-pilot/theme/ts/extend.ts
@@ -0,0 +1,63 @@
+export default {
+    typography: {
+        font-size: {
+            12: "12px",
+            14: "14px",
+            16: "16px",
+            18: "18px",
+            20: "20px",
+            24: "24px",
+            32: "32px",
+            48: "48px"
+        },
+        line-height: {
+            18: "18px",
+            20: "20px",
+            24: "24px",
+            28: "28px",
+            36: "36px",
+            40: "40px",
+            58: "58px"
+        }
+    },
+    bulk transcription modal: {
+        color: "{overlay.modal.color}",
+        title: {
+            font: {
+                size: "1.25rem",
+                weight: "600px"
+            }
+        },
+        border: {
+            color: "{overlay.modal.border.color}",
+            radius: "{overlay.modal.border.radius}"
+        },
+        footer: {
+            gap: "0.5rem",
+            padding: {
+                top: "0",
+                left: "{overlay.modal.padding}",
+                right: "{overlay.modal.padding}",
+                bottom: "{overlay.modal.padding}"
+            }
+        },
+        header: {
+            gap: "0.5rem",
+            padding: "{overlay.modal.padding}"
+        },
+        content: {
+            padding: {
+                top: "0",
+                left: "{overlay.modal.padding}",
+                right: "{overlay.modal.padding}",
+                bottom: "{overlay.modal.padding}"
+            }
+        },
+        subheader: {
+            gap: "0.5rem",
+            color: "{form.field.float.label.color}",
+            padding: "{overlay.modal.padding}"
+        },
+        background: "{overlay.modal.background}"
+    }
+}
